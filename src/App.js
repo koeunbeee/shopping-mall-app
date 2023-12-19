50aa2a6d0ae2d1a5b1dd7deaa01268eae9a3c66d
Bella <kbee1202@gmail.com>
2023-12-19 11:37:02 +0900

    리액트 라우터 돔 및 라우터 오류 해결 및 루트 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { Outlet, Routes, Route } from 'react-router-dom';
+
 import React from 'react';
 import Nav from './component/Nav';
 import Login from './component/Login';
@@ -6,15 +8,26 @@ import ProductsList from './component/ProductsList';
 import Product from './component/Product';
 import Cart from './component/Cart';
 
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+      <header className="header"></header>
+      <Outlet />
+    </div>
+  );
+};
 function App() {
   return (
     <div className="app-container">
-      <Nav />
-      <ProductsList />
-      <Login />
-      <Cart />
-      <Product />
-      <header className="header"></header>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="productlist" element={<ProductsList />} />
+          <Route path="product" element={<Product />} />
+          <Route path="login" element={<Login />} />
+          <Route path="cart" element={<Cart />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
